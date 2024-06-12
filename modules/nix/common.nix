e01de941a0970ccbc1ce65cad80be4cc0487a984
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2024-06-12 20:19:11 +0100

    feat: fix keyboard language

--- modules/nix/common.nix
+++ modules/nix/common.nix
@@ -9,8 +9,9 @@
   networking.hostName = lib.mkForce host;
   nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
   time.timeZone = "Europe/London";
-  services.xserver.
-    layout = "uk,us";
+  services.xserver.layout = "gb,us";
+  services.xserver.xkb.layout = "gb,us";
+  console.keyMap = "uk";
 
   # Make this slightly higher priority than whatever the default is but pretty much always overrideable
   system.stateVersion = lib.mkOverride 1499 defaultStateVersion;
