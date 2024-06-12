38a52aa59014ad2019dfe71582ca1d5e9776ae58
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2024-06-12 20:45:19 +0100

    fix: format Cerberus hardware config

--- hosts/cerberus/hardware-configuration.nix
+++ hosts/cerberus/hardware-configuration.nix
@@ -1,8 +1,12 @@
-{inputs,lib,...}: {
+{
+  inputs,
+  lib,
+  ...
+}: {
   imports = ["${inputs.nixpkgs}/nixos/modules/installer/virtualbox-demo.nix"];
   nix.settings.trusted-users = ["demo"];
-  services.xserver={
-    desktopManager.plasma5.enable=lib.mkForce true;
-    displayManager.sddm.enable=lib.mkForce true;
+  services.xserver = {
+    desktopManager.plasma5.enable = lib.mkForce true;
+    displayManager.sddm.enable = lib.mkForce true;
   };
 }
