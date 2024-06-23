8e43a8c48371d838e32f987e5838694f169fc29f
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2024-06-23 15:26:16 +0100

    feat: add commit scopes

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,1 +1,4 @@
-export default { extends: ["@commitlint/config-conventional"] };
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: { "scope-enum": [2, "always", ["readme", "user", "host"]] },
+};
