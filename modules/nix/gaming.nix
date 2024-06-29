8bd87f631690ed63e4803d55a6bccd568238934a
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2024-06-29 21:59:53 +0100

    fix: block steam firewalls

--- modules/nix/gaming.nix
+++ modules/nix/gaming.nix
@@ -5,7 +5,11 @@
   ...
 }: {
   config = lib.mkIf (opts.gaming.enable && opts.GUI.enable) {
-    programs.steam.enable = true;
+    programs.steam = {
+      enable = true;
+      remotePlay.openFirewall = false;
+      dedicatedServer.openFirewall = false;
+    };
     unfreePackages = [
       "steam"
       "steam-original"
