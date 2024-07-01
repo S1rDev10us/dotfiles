5ca40a2e7881a3de7261ebcddf3d4bec73e5004e
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2024-07-01 01:47:33 +0100

    fix: disable grub on WSL

--- modules/nix/old/grub.nix
+++ modules/nix/old/grub.nix
@@ -1,4 +1,9 @@
-{...}: {
+{
+  lib,
+  opts,
+  ...
+}:
+lib.mkIf (!opts.WSL.enable) {
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
