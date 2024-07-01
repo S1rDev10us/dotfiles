643059b43a5374f212ea4f56d0d947c65e5eab89
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2024-07-01 01:45:45 +0100

    fix(host): update cerberus to be 24.05 compatible

--- hosts/cerberus/hardware-configuration.nix
+++ hosts/cerberus/hardware-configuration.nix
@@ -5,8 +5,11 @@
 }: {
   imports = ["${inputs.nixpkgs}/nixos/modules/installer/virtualbox-demo.nix"];
   nix.settings.trusted-users = ["demo"];
-  services.xserver = {
-    desktopManager.plasma5.enable = lib.mkForce true;
+
+  services = {
+    xserver = {
+      desktopManager.plasma5.enable = lib.mkForce true;
+    };
     displayManager.sddm.enable = lib.mkForce true;
   };
 }
