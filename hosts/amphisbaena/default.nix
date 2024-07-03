05c1f962ec0b618a0bf8d2524d7f4a37c522b27b
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2024-07-03 07:08:09 +0100

    feat(host): add amphisbaena files

---
+++ hosts/amphisbaena/default.nix
@@ -0,0 +1,13 @@
+{
+  stateVersion = "24.05";
+  coding.enable = true;
+  # environment.hyprland.enable = true;
+  fun.enable = true;
+  gamedev.enable = true;
+  gaming.enable = true;
+  GUI.enable = true;
+  users.s1rdev10us = {
+    enable = true;
+    admin = true;
+  };
+}
