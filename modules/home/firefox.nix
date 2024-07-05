e451fd7f2b3baa202049023fcaf309274f2692d7
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2024-07-05 12:29:57 +0100

    fix: configure firefox search engines correctly

--- modules/home/firefox.nix
+++ modules/home/firefox.nix
@@ -7,24 +7,35 @@
 lib.mkIf opts.GUI.enable {
   programs.firefox = {
     enable = true;
-    profiles."${user}" = {
+    profiles.default = {
       isDefault = true;
       name = user;
+      search = {
+        default = "DuckDuckGo";
+        force = true;
+        engines = {
+          Bing.metaData.hidden = true;
+          Google.metaData.hidden = true;
+          eBay.metaData.hidden = true;
+        };
+      };
     };
     policies = {
+      CaptivePortal = false;
       DisableAppUpdate = true;
       DisableFirefoxStudies = true;
       DisablePocket = true;
       DisableSetDesktopBackground = true;
       DisableTelemetry = true;
+      FirefoxHome = {
+        TopSites = false;
+        Highlights = false;
+        Pocket = false;
+        Snippets = false;
+      };
       HttpsOnlyMode = "enabled";
       NoDefaultBookmarks = true;
       OfferToSaveLogins = false;
-      SearchEngines = {
-        Add = [];
-        Remove = ["Google" "Bing" "eBay"];
-        Default = "DuckDuckGo";
-      };
     };
   };
 }
