dbbead44651f08e44f732e7fae4aa5311980788a
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2024-07-07 19:35:47 +0100

    fix: add fastfetch config and fix missing bashrc issue

--- modules/home/fun.nix
+++ modules/home/fun.nix
@@ -6,7 +6,62 @@
   ...
 }: {
   config = lib.mkIf opts.fun.enable {
-    programs.bash.bashrcExtra = "fastfetch";
-    home.packages = [pkgs.fastfetch];
+    programs.bash = {
+      enable = true;
+      bashrcExtra = "fastfetch";
+    };
+    programs.fastfetch = {
+      enable = true;
+      settings = {
+        "display" = {
+          "size" = {
+            "maxPrefix" = "MB";
+            "ndigits" = 0;
+          };
+        };
+        "modules" = [
+          "title"
+          "separator"
+          "os"
+          "host"
+          "kernel"
+          "uptime"
+          "packages"
+          "shell"
+          {
+            "type" = "display";
+            "compactType" = "original";
+            "key" = "Resolution";
+          }
+          "de"
+          "wm"
+          "wmtheme"
+          "theme"
+          "icons"
+          "font"
+          "cursor"
+          "terminal"
+          {
+            "type" = "terminalfont";
+            "format" = "{/2}{-}{/}{2}{?3} {3}{?}";
+          }
+          "cpu"
+          {
+            "type" = "gpu";
+            "key" = "GPU";
+          }
+          {
+            "type" = "memory";
+            "format" = "{/1}{-}{/}{/2}{-}{/}{} / {}";
+          }
+          "swap"
+          "disk"
+          "battery"
+          "locale"
+          "break"
+          "colors"
+        ];
+      };
+    };
   };
 }
