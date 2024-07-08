9e5dccf3f9935dada1b275ef8749d59bb9ae85fe
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2024-07-08 12:42:29 +0100

    fix: make nvim the default editor

--- modules/home/coding.nix
+++ modules/home/coding.nix
@@ -7,6 +7,7 @@
 lib.mkIf opts.coding.enable {
   programs.neovim = {
     enable = true;
+    defaultEditor = true;
     # Mason dependencies
     extraPackages = with pkgs; [nodejs_22 dotnetCorePackages.dotnet_8.sdk unzip rustup wget];
   };
