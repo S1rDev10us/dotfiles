67bc5762bc8bf118d23e543b1b216e3460a5e63a
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2024-08-01 12:34:49 +0100

    fix: enable avahi for printer location

--- modules/nix/old/printing.nix
+++ modules/nix/old/printing.nix
@@ -1,4 +1,11 @@
 {...}: {
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+  services = {
+    # Enable CUPS to print documents.
+    printing.enable = true;
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
+  };
 }
