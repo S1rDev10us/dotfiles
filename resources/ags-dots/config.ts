4ae1b94eee2d5c68b3bf3435d01dbc2f83ca561a
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2024-08-08 11:03:18 +0100

    fix: automatically create new bars for new monitors

--- resources/ags-dots/config.ts
+++ resources/ags-dots/config.ts
@@ -1,6 +1,36 @@
+const hyprland = await Service.import("hyprland");
+import type { Monitor } from "types/service/hyprland";
 import { Bar } from "widgets/bar/index";
 
+const bars = {};
+
 App.config({
-  windows: [Bar(0)],
   style: "./style.css",
 });
+
+function addMonitor(monitor: number) {
+  if (bars[monitor]) return;
+  bars[monitor] = Bar(monitor);
+  App.addWindow(bars[monitor]);
+}
+function removeMonitor(monitor: number) {
+  if (!bars[monitor]) {
+    console.warn(
+      "Warning! monitor was removed but the bar for it does not exist",
+    );
+    return;
+  }
+  App.removeWindow(bars[monitor]);
+  bars[monitor] = undefined;
+}
+
+for (const monitor of hyprland.monitors) {
+  addMonitor(monitor.id);
+}
+
+hyprland.connect("monitor-added", (_, monitor: Monitor) =>
+  addMonitor(monitor.id),
+);
+hyprland.connect("monitor-removed", (_, monitor: Monitor) =>
+  removeMonitor(monitor.id),
+);
