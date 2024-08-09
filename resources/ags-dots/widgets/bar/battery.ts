c55fb276b7d5c678f5fc61d36ca3054848badf2d
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2024-08-09 03:49:05 +0100

    fix(ags): manually generate battery icon

--- resources/ags-dots/widgets/bar/battery.ts
+++ resources/ags-dots/widgets/bar/battery.ts
@@ -3,13 +3,30 @@ const battery = await Service.import("battery");
 export const BatteryProgress = () =>
   Widget.CircularProgress({
     child: Widget.Icon({
-      icon: battery.bind("icon_name").as((icon) => {
-        const symbolic = "-symbolic";
-        if (icon.endsWith(symbolic)) {
-          icon = icon.substring(0, icon.length - symbolic.length);
-        }
-        return icon;
-      }),
+      icon: Utils.merge(
+        [battery.bind("charging"), battery.bind("percent")],
+        (charging, percent) => {
+          let icon = "battery-";
+
+          // I have battery set to max out at 80/79 percent to help with battery health so we respect that here
+          if (percent > 75) {
+            icon += "full";
+          } else if (percent > 55) {
+            icon += "good";
+          } else if (percent > 25) {
+            icon += "low";
+          } else if (percent > 10) {
+            icon += "caution";
+          } else {
+            icon += "empty";
+          }
+
+          if (charging) {
+            icon += "-charging";
+          }
+          return icon;
+        },
+      ),
     }),
     visible: battery.bind("available"),
     value: battery.bind("percent").as((p) => (p > 0 ? p / 100 : 0)),
