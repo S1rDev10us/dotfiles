ac1fee1c79c92d8d7d5fbde430c83dc724a8af58
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2024-08-13 20:06:07 +0100

    fix: the wsl import conflicts with nixos-hardware so it should be disabled by default

--- modules/nix/WSL.nix
+++ modules/nix/WSL.nix
@@ -1,20 +1,21 @@
 {
   inputs,
-  config,
   lib,
-  pkgs,
   opts,
   ...
 }: {
-  imports = [inputs.nixos-wsl.nixosModules.default];
-  config = lib.mkIf opts.WSL.enable {
-    programs.nix-ld = {
-      enable = true;
-      # package = pkgs.nix-ld-rs;
-    };
-    wsl = {
-      enable = true;
-      defaultUser = "nixos";
-    };
-  };
+  imports = lib.optional opts.WSL.enable inputs.nixos-wsl.nixosModules.default;
+  config =
+    if opts.WSL.enable
+    then {
+      programs.nix-ld = {
+        enable = true;
+        # package = pkgs.nix-ld-rs;
+      };
+      wsl = {
+        enable = true;
+        defaultUser = "nixos";
+      };
+    }
+    else {};
 }
