bc8c9fbb6a00fcfb54717ea734da07520d882cc0
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2024-08-13 18:28:12 +0100

    fix: add support for adding more firefox profiles and ensuring they have the correct values set

--- modules/home/firefox.nix
+++ modules/home/firefox.nix
@@ -7,33 +7,43 @@
 lib.mkIf opts.GUI.enable {
   programs.firefox = {
     enable = true;
-    profiles.default = {
-      isDefault = true;
-      name = user;
-      search = {
-        default = "DuckDuckGo";
-        force = true;
-        engines = {
-          Bing.metaData.hidden = true;
-          Google.metaData.hidden = true;
-          eBay.metaData.hidden = true;
-          Github = {
-            urls = [
-              {
-                template = "https://github.com/search";
-                params = [
-                  {
-                    name = "q";
-                    value = "{searchTerms}";
-                  }
-                ];
-              }
-            ];
-            iconUpdateUrl = "https://github.com/favicon.ico";
-            updateInterval = 24 * 60 * 60 * 1000; # every day
-            aliases = ["@gh"];
+    profiles = let
+      baseSettings = {
+        search = {
+          default = "DuckDuckGo";
+          force = true;
+          engines = {
+            Bing.metaData.hidden = true;
+            Google.metaData.hidden = true;
+            eBay.metaData.hidden = true;
+            Github = {
+              urls = [
+                {
+                  template = "https://github.com/search";
+                  params = [
+                    {
+                      name = "q";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+              iconUpdateUrl = "https://github.com/favicon.ico";
+              updateInterval = 24 * 60 * 60 * 1000; # every day
+              aliases = ["@gh"];
+            };
           };
         };
+      };
+      settingsFrom = extraSettings: let
+        result = lib.recursiveUpdate baseSettings extraSettings;
+        hasName = builtins.hasAttr "name" result;
+      in
+        lib.throwIf hasName "All firefox profiles MUST have a name assigned" result;
+    in {
+      default = settingsFrom {
+        name = user;
+        isDefault = true;
       };
     };
     policies = {
