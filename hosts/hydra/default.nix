37903b14d16086d00fcb7c563dccbf26aab3fada
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2024-08-13 20:46:59 +0100

    fix(host): always enable hyprland on hydra

--- hosts/hydra/default.nix
+++ hosts/hydra/default.nix
@@ -1,8 +1,6 @@
-{env, ...}: {
-  # environment.gnome.enable = true;
-  # Temporarily manage hyprland imperatively until I get home manager sorted for hyprland
-  environment.hyprland.enable = env == "nixos";
+{
   stateVersion = "23.11";
+  environment.hyprland.enable = true;
   GUI.enable = true;
   coding.enable = true;
   users.s1rdev10us = {
