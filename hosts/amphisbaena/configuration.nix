856f873a262793465cdcc8189e7d4a75b4c36407
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2024-09-10 11:41:30 +0100

    fix(host): only use kernel version 6.8 if the default is older than that

--- hosts/amphisbaena/configuration.nix
+++ hosts/amphisbaena/configuration.nix
@@ -1,10 +1,11 @@
 {
   inputs,
   pkgs,
+  lib,
   ...
 }: {
   # FastFetch reports my host as `UX363EA_UX371EA` so I think this is correct, it should be close enough anyway I think
   imports = [inputs.nixos-hardware.nixosModules.asus-zenbook-ux371];
   # There's theoretically a fix for my speakers in a 6.7 kernel upgrade
-  boot.kernelPackages = pkgs.linuxPackages_6_8;
+  boot.kernelPackages = lib.mkIf (lib.versionOlder pkgs.linux.version "6.8") pkgs.linuxPackages_6_8;
 }
