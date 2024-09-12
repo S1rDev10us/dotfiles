97abbf2ba95ab571996168ae58138bec918c3941
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2024-09-12 22:47:22 +0100

    feat(ags): update media controls

--- resources/ags-dots/widgets/bar/media.ts
+++ resources/ags-dots/widgets/bar/media.ts
@@ -3,19 +3,44 @@ import type { MprisPlayer } from "types/service/mpris";
 const mpris = await Service.import("mpris");
 
 const Player = (player: MprisPlayer) =>
-  Widget.Button({
-    css: player
-      .bind("track_cover_url")
-      .as(
-        (url) =>
-          `background-image:url('${url}');background-size: cover;background-position:center;`,
-      ),
-
-    on_clicked: () => player.playPause(),
-    label: player.bind("track_title"),
+  Widget.Box({
+    children: [
+      Widget.Button({
+        on_clicked: () => player.previous(),
+        child: Widget.Icon("media-skip-backward"),
+      }),
+      Widget.Button({
+        on_clicked: () => player.playPause(),
+        child: Widget.Box({
+          children: [
+            Widget.Icon({
+              icon: player
+                .bind("play_back_status")
+                .as((status) =>
+                  status == "Playing"
+                    ? "media-playback-pause"
+                    : "media-playback-start",
+                ),
+            }),
+            Widget.Icon({
+              className: "media-icon",
+              icon: player
+                .bind("track_cover_url")
+                .as((url) => (url.startsWith("file://") ? url.slice(7) : url)),
+            }),
+            Widget.Label({ label: player.bind("track_title") }),
+          ],
+        }),
+      }),
+      Widget.Button({
+        on_clicked: () => player.next(),
+        child: Widget.Icon("media-skip-forward"),
+      }),
+    ],
   });
 
 export const Players = () =>
   Widget.Box({
+    className: "horizontal mediaPlayers",
     children: mpris.bind("players").as((players) => players.map(Player)),
   });
