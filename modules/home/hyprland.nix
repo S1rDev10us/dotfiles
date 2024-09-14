37514a2cc7b88bad1121ed8d96cc7a78bf2aa53a
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2024-09-14 10:26:30 +0100

    fix: make hypridle timeout definition more concise

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -72,33 +72,35 @@
             lock_cmd = "pidof hyprlock || hyprlock";
           };
 
-          listener = [
+          listener = let
+            minute = 60;
+          in [
             {
-              timeout = 150; # 2.5min.
+              timeout = 2.5 * minute;
               on-timeout = "brightnessctl -s set 10"; # set monitor backlight to minimum, avoid 0 on OLED monitor.
               on-resume = "brightnessctl -r"; # monitor backlight restore.
             }
 
             # turn off keyboard backlight, comment out this section if you dont have a keyboard backlight.
             {
-              timeout = 150; # 2.5min.
+              timeout = 2.5 * minute;
               on-timeout = "brightnessctl -sd rgb:kbd_backlight set 0"; # turn off keyboard backlight.
               on-resume = "brightnessctl -rd rgb:kbd_backlight"; # turn on keyboard backlight.
             }
 
             {
-              timeout = 300; # 5min
+              timeout = 5 * minute;
               on-timeout = "loginctl lock-session"; # lock screen when timeout has passed
             }
 
             {
-              timeout = 330; # 5.5min
+              timeout = 5.5 * minute;
               on-timeout = "hyprctl dispatch dpms off"; # screen off when timeout has passed
               on-resume = "hyprctl dispatch dpms on"; # screen on when activity is detected after timeout has fired.
             }
 
             {
-              timeout = 1800; # 30min
+              timeout = 30 * minute;
               on-timeout = "systemctl suspend"; # suspend pc
             }
           ];
