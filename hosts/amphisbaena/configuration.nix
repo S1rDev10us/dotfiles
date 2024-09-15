12cf49cf53960afa5c8f6d6b7ad7d96e0d9c6910
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2024-09-15 10:32:41 +0100

    feat: update packages

--- hosts/amphisbaena/configuration.nix
+++ hosts/amphisbaena/configuration.nix
@@ -7,5 +7,5 @@
   # FastFetch reports my host as `UX363EA_UX371EA` so I think this is correct, it should be close enough anyway I think
   imports = [inputs.nixos-hardware.nixosModules.asus-zenbook-ux371];
   # There's theoretically a fix for my speakers in a 6.7 kernel upgrade
-  boot.kernelPackages = lib.mkIf (lib.versionOlder pkgs.linux.version "6.8") pkgs.linuxPackages_6_8;
+  boot.kernelPackages = lib.mkIf (lib.versionOlder pkgs.linux.version "6.8") pkgs.linuxPackages_6_10;
 }
