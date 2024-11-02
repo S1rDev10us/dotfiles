61cde563ddfa2d7032c9ef1c85e924f77b5eb25d
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2024-11-02 12:58:29 +0000

    fix: add cls command to amphisbaena

--- hosts/amphisbaena/home.nix
+++ hosts/amphisbaena/home.nix
@@ -1,8 +1,18 @@
-{lib, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   wayland.windowManager.hyprland.settings = {
     # Generated using `gtf` https://old.reddit.com/r/hyprland/comments/18fb0zp/looking_for_help_with_resolution/kcypu7w/
     monitor = lib.mkBefore ["eDP-1, modeline 172.80 1920 2040 2248 2576 1080 1081 1084 1118 -hsync +vsync, 0x0, 1"];
 
     exec-once = ["[workspace 1 silent] foot -D ~/Documents/repos/pvp_spaceship_game/"];
   };
+  home.packages = [
+    (pkgs.writeShellScriptBin "cls" ''
+      clear
+      fastfetch
+    '')
+  ];
 }
