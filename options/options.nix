db70c402fede9e27ebece714a8fc75ab28bcecbd
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2024-12-06 13:41:20 +0000

    fix: sort option keys alphabetically

--- options/options.nix
+++ options/options.nix
@@ -8,10 +8,6 @@ with lib; let
   enable = option: option // {default = true;};
 in {
   options = {
-    stateVersion = mkOption {
-      type = types.str;
-      default = "23.11";
-    };
     batteryManagement.enable = mkEnableOption "battery management";
     coding.enable = mkEnableOption "code editors";
     environment = {
@@ -32,8 +28,12 @@ in {
     };
     # Automatically enables the default if one of the desktop environments (/window manager) is enabled
     GUI.enable = (mkEnableOption "gui packages") // {default = with config.environment; lib.any (x: x) [hyprland.enable gnome.enable KDE.enable];};
-    WSL.enable = mkEnableOption "WSL";
+    stateVersion = mkOption {
+      type = types.str;
+      default = "23.11";
+    };
     VM.enable = mkEnableOption "Virtual Box guest support";
+    WSL.enable = mkEnableOption "WSL";
     users = lib.genAttrs (libx.allFrom ../users) (user: {
       enable = mkEnableOption "${user} user";
       admin = mkEnableOption "${user} admin";
