e2985dfc085ad669d2791c8e013e644e7f11e1d2
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2024-12-06 13:19:13 +0000

    fix: remove obsolete waybar config

--- modules/home/waybar.nix
+++ modules/home/waybar.nix
@@ -36,46 +36,6 @@ lib.mkIf opts.environment.hyprland.enable {
             "ꯍꯍꯍ" = "";
           };
         };
-        "hyprland/workspaces" = {
-          format = "{icon} {windows} ";
-          format-icons =
-            (builtins.listToAttrs (builtins.genList (x: let
-                y = x + 1;
-              in {
-                name = "${builtins.toString y}";
-                value = "${builtins.toString y}  ";
-              })
-              10))
-            // {
-              "2" = "2 󰈹";
-              active = "  ";
-            };
-          format-window-separator = " ";
-          window-rewrite-default = "";
-          window-rewrite = {
-            "class<foot>" = "";
-            "title<.*NVIM>" = "";
-            "class<firefox>" = "󰈹";
-            "class<obsidian>" = "";
-          };
-        };
-        "custom/power" = {
-          format = "⏻ ";
-          tooltip = false;
-          menu = "on-click";
-          menu-file = "${
-            builtins.fetchurl {
-              url = "https://github.com/Alexays/Waybar/raw/44f39ca0ce53659df2c959fa9177cfe158f23273/resources/custom_modules/power_menu.xml";
-              sha256 = "sha256:0yi9vrmlv721dm1a3k0i6c8a4whiqx4hpxsr1f0z6gnjrf9lgpd1";
-            }
-          }";
-          menu-actions = {
-            shutdown = "shutdown now";
-            reboot = "reboot";
-            suspend = "systemctl suspend";
-            hibernate = "systemctl hibernate";
-          };
-        };
       }
     ];
   };
