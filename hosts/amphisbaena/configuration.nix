a28207858ec8c5e60c919272674bbd4ead2d6bdf
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2025-03-16 10:43:41 +0000

    feat: move plasma to specialisation on amphisbaena

--- hosts/amphisbaena/configuration.nix
+++ hosts/amphisbaena/configuration.nix
@@ -2,6 +2,7 @@
   inputs,
   pkgs,
   lib,
+  config,
   ...
 }: {
   # FastFetch reports my host as `UX363EA_UX371EA` so I think this is correct, it should be close enough anyway I think
@@ -9,4 +10,10 @@
   # There's theoretically a fix for my speakers in a 6.7 kernel upgrade
   boot.kernelPackages = lib.mkIf (lib.versionOlder pkgs.linux.version "6.8") pkgs.linuxPackages_6_11;
   services.avahi.enable = true;
+  services.desktopManager.plasma6.enable = lib.mkForce false;
+  specialisation.plasma.configuration = {
+    services.desktopManager.plasma6.enable = lib.mkOverride 49 true;
+    programs.hyprland.enable = lib.mkForce false;
+    services.displayManager.defaultSession = lib.mkOverride 49 "plasma";
+  };
 }
