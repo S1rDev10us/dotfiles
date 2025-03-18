724380f939f6882efe6f904d27cd0d7e093819a6
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2025-03-18 13:46:34 +0000

    feat: remove unused xserver channel

--- modules/nix/KDE.nix
+++ modules/nix/KDE.nix
@@ -7,7 +7,6 @@
 lib.mkIf (opts.environment.KDE.enable && opts.GUI.enable) {
   services = {
     # This lets you use x11 as an escape hatch if wayland isn't working for some reason
-    xserver.enable = true;
     displayManager.sddm.enable = true;
     desktopManager.plasma6.enable = true;
   };
