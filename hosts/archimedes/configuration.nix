fe83a952ef718a773f90fe6423074a23de81cbc8
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2025-03-29 15:08:52 +0000

    fix: archimedes k3s cgroup memory fix

--- hosts/archimedes/configuration.nix
+++ hosts/archimedes/configuration.nix
@@ -9,6 +9,11 @@
   boot.loader.grub.enable = lib.mkForce false;
   boot.loader.generic-extlinux-compatible.enable = true;
 
+  # k3s cgroup memory issue
+  # https://github.com/k3d-io/k3d/discussions/854
+  # https://old.reddit.com/r/NixOS/comments/10huogm/k3s_does_not_find_memory_cgroup_v2/
+  boot.kernelParams = ["cgroup_enable=memory" "cgroup_enable=cpuset" "cgroup_memory=1"];
+
   environment.systemPackages = with pkgs; [
     vim
   ];
