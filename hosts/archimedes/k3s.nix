b020ebfe144566165db184ab929c1de39aec4efa
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2025-03-29 14:36:23 +0000

    fix: allow k3s proxy port

--- hosts/archimedes/k3s.nix
+++ hosts/archimedes/k3s.nix
@@ -1,6 +1,7 @@
 {
   networking.firewall.allowedTCPPorts = [
     6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
+    8001 # k3s proxy port
     # 2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
     # 2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
   ];
