7a550ab6fdf5f355ba2f723af6cd1079f5191b6a
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2025-10-18 16:01:16 +0100

    fix: remove Thunar

--- modules/nix/programs/fileNavigation.nix
+++ modules/nix/programs/fileNavigation.nix
@@ -3,10 +3,8 @@
   config,
   ...
 }: {
-  programs.thunar.enable = true;
   xdg.icons.enable = true;
   # Automount
   services.udisks2.enable = true;
   # Trash, ... support in thunar
-  services.gvfs.enable = true;
 }
