5ab2caf0794e4d2e9ce92cc64ffe0661b7637d94
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2025-10-18 16:02:46 +0100

    feat: reduce swappiness and remove blueman from amphisbaena

--- hosts/amphisbaena/configuration.nix
+++ hosts/amphisbaena/configuration.nix
@@ -2,7 +2,6 @@
   inputs,
   pkgs,
   lib,
-  config,
   ...
 }: {
   # FastFetch reports my host as `UX363EA_UX371EA` so I think this is correct, it should be close enough anyway I think
@@ -13,11 +12,13 @@
   services.avahi.enable = true;
 
   boot.kernel.sysctl = {
-    "vm.swappiness" = 40;
+    "vm.swappiness" = 20;
   };
 
   # nixos-hardware options
   hardware.asus.battery.chargeUpto = 80;
+
+  services.blueman.enable = lib.mkForce false;
   services.postgresql = {
     ensureDatabases = ["s1rdev10us"];
     ensureUsers = [
