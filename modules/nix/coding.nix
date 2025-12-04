b57f92f95b72b0f79cb69d6cd430bc13b139eb55
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2025-12-04 22:29:12 +0000

    feat: remove unity and jetbrains

--- modules/nix/coding.nix
+++ modules/nix/coding.nix
@@ -10,29 +10,8 @@
       ripgrep
     ]
     ++ lib.optionals opts.GUI [
-      (
-        unityhub.override {
-          extraLibs = pkgs: [pkgs.openssl_1_1];
-        }
-      )
-      jetbrains-toolbox
-      # These are needed for Jetbrains Rider to work with unity. Alternatively they could be provided only to Rider using something like [this](https://huantian.dev/blog/unity3d-rider-nixos/) [configuration](https://github.com/huantianad/nixos-config/blob/main/modules/editors/rider.nix)
-      # dotnetCorePackages.sdk_6_0
-      dotnetCorePackages.dotnet_9.sdk
-      dotnetPackages.Nuget
-      mono
-      # msbuild
       waypipe
     ];
-  permittedInsecurePackages = [
-    "openssl-1.1.1w"
-  ];
-  unfreePackages = lib.optionals opts.GUI (with pkgs; [
-    corefonts
-    unityhub
-    jetbrains-toolbox
-    jetbrains.rider
-  ]);
   programs = {
     git.enable = true;
     nix-ld = {
