4d06ab1eaf9300b0065dc179e9d59b5754173139
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2026-01-14 17:29:25 +0000

    feat: replace tlp with auto-cpufreq

--- modules/nix/batteryManagement.nix
+++ modules/nix/batteryManagement.nix
@@ -1,26 +1,17 @@
 {
   services.power-profiles-daemon.enable = false;
   services.thermald.enable = true;
-  services.tlp = {
+  services.auto-cpufreq = {
     enable = true;
     settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-
-      CPU_MIN_PERF_ON_AC = 0;
-      CPU_MAX_PERF_ON_AC = 100;
-      CPU_MIN_PERF_ON_BAT = 0;
-      CPU_MAX_PERF_ON_BAT = 50;
-
-      # Battery threshold
-      START_CHARGE_THRESH_BAT0 = 60;
-      STOP_CHARGE_THRESH_BAT0 = 80;
-
-      CPU_BOOST_ON_AC = 1;
-      CPU_BOOST_ON_BAT = 0;
+      battery = {
+        governer = "powersave";
+        turbo = "never";
+      };
+      charger = {
+        governer = "performance";
+        turbo = "auto";
+      };
     };
   };
   powerManagement.powertop.enable = true;
