dbfcaf80dad08e4e470f1d10da8ea5e33023a62b
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2026-01-14 17:28:41 +0000

    feat: add wlx-overlay-s for VR

--- modules/nix/programs/VR.nix
+++ modules/nix/programs/VR.nix
@@ -1,7 +1,8 @@
-{lib, ...}: {
+{pkgs, ...}: {
   programs.alvr = {
     enable = true;
     openFirewall = true;
   };
   programs.steam.remotePlay.openFirewall = true;
+  environment.systemPackages = with pkgs; [wlx-overlay-s];
 }
