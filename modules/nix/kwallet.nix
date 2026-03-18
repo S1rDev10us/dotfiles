251956cb27d875753c42e6bc1b15dc2bf28acca3
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2026-03-18 11:37:13 +0000

    fix: correct kwallet pam services

--- modules/nix/kwallet.nix
+++ modules/nix/kwallet.nix
@@ -2,25 +2,20 @@
   pkgs,
   opts,
   lib,
-  config,
   ...
-}: let
-  notInKDE = !config.toggles.windowManager.KDE.enable;
-in {
+}: {
   # Use kwallet for a secret store
   environment.systemPackages = with pkgs;
   with kdePackages;
     [kwallet kwallet-pam]
     ++ (lib.optional opts.GUI kwalletmanager);
-  xdg.portal.extraPortals = lib.mkIf notInKDE [
+  xdg.portal.extraPortals = [
     pkgs.kdePackages.kwallet
   ];
   # Auto unlock with pam
-  security.pam.services = lib.mkIf (notInKDE && opts.users.s1rdev10us.enable) {
-    s1rdev10us.kwallet = {
-      enable = true;
-      package = pkgs.kdePackages.kwallet-pam;
-    };
+  security.pam.services = {
+    login.kwallet.enable = true;
+    sddm.kwallet.enable = true;
   };
   environment.sessionVariables.GIT_ASKPASS = "${pkgs.kdePackages.ksshaskpass.out}/bin/ksshaskpass";
 }
