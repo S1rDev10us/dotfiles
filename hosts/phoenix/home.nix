fd662b5b476175acdd0a601ed133511beb313755
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2026-04-19 15:01:16 +0100

    fix: use correct type for programs.nixi.extraRules

--- hosts/phoenix/home.nix
+++ hosts/phoenix/home.nix
@@ -4,13 +4,15 @@
   ...
 }: {
   programs.helix.defaultEditor = true;
-  programs.niri.extraRules = ''
-    input {
-      tablet {
-        map-to-output "DP-2"
+  programs.niri.extraRules = [
+    ''
+      input {
+        tablet {
+          map-to-output "DP-2"
+        }
       }
-    }
-  '';
+    ''
+  ];
   home.packages = [
     inputs.tagstudio.packages.${pkgs.stdenv.hostPlatform.system}.tagstudio-jxl
   ];
