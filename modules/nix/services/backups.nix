16058ddfc284053689666245b5dae91a10236f92
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2026-05-13 19:51:38 +0100

    fix: add backup-list command and reduce duplication of borg env variables

--- modules/nix/services/backups.nix
+++ modules/nix/services/backups.nix
@@ -8,6 +8,10 @@
   ...
 }: let
   hostname = config.networking.hostName;
+  remote = "borgbackup@archimedes";
+  repo = "${remote}:/home/borgbackup/borg/${hostname}";
+  BORG_RSH = "${pkgs.openssh}/bin/ssh -i /etc/borg-backup/id_${hostname}_borgbackup";
+  BORG_PASSCOMMAND = "cat /etc/borg-backup/${hostname}_borgbackup_passphrase";
 in {
   services.borgbackup.jobs.backupToArchimedes = {
     paths = ["/home" "/etc" "/var/lib"];
@@ -92,12 +96,12 @@ in {
     extraCreateArgs = "--stats";
     extraPruneArgs = "--stats";
     failOnWarnings = true;
-    repo = "borgbackup@archimedes:/home/borgbackup/borg/${hostname}";
+    inherit repo;
     encryption = {
       mode = "repokey";
-      passCommand = "cat /etc/borg-backup/${hostname}_borgbackup_passphrase";
+      passCommand = BORG_PASSCOMMAND;
     };
-    environment.BORG_RSH = "${pkgs.openssh}/bin/ssh -i /etc/borg-backup/id_${hostname}_borgbackup";
+    environment.BORG_RSH = BORG_RSH;
     # manually set to true initially
     doInit = lib.mkDefault false;
     compression = "auto,lzma";
@@ -106,26 +110,33 @@ in {
       until ${outputs.packages.${pkgs.stdenv.hostPlatform.system}.onhomenetwork}/bin/onhomenetwork.bash; do sleep 10; done
     '';
   };
-  environment.systemPackages = [
-    (pkgs.writeShellScriptBin "backup-listen" ''
-      journalctl -u borgbackup-job-backupToArchimedes.service -f -o short-iso --no-hostname
-    '')
-    (pkgs.writeShellScriptBin "backup-history" ''
-      journalctl -u borgbackup-job-backupToArchimedes.service -o short-iso --no-hostname
-    '')
-    (pkgs.writeShellScriptBin "backup-start" ''
-      systemctl restart borgbackup-job-backupToArchimedes.service
-    '')
-    (pkgs.writeShellScriptBin "backup-status" ''
-      systemctl status borgbackup-job-backupToArchimedes.service
-    '')
-
-    (pkgs.writeShellScriptBin "backup-mount" ''
-      BORG_PASSCOMMAND="cat /etc/borg-backup/${hostname}_borgbackup_passphrase" BORG_RSH="ssh -i /etc/borg-backup/id_${hostname}_borgbackup" doas borg mount borgbackup@archimedes:/home/borgbackup/borg/${hostname} /mnt/backup
-    '')
-    (pkgs.writeShellScriptBin "backup-umount" ''
-      doas borg umount /mnt/backup
-      doas rmdir /mnt/backup
-    '')
-  ];
+  environment.systemPackages =
+    [
+      (pkgs.writeShellScriptBin "backup-listen" ''
+        journalctl -u borgbackup-job-backupToArchimedes.service -f -o short-iso --no-hostname
+      '')
+      (pkgs.writeShellScriptBin "backup-history" ''
+        journalctl -u borgbackup-job-backupToArchimedes.service -o short-iso --no-hostname
+      '')
+      (pkgs.writeShellScriptBin "backup-start" ''
+        systemctl restart borgbackup-job-backupToArchimedes.service
+      '')
+      (pkgs.writeShellScriptBin "backup-status" ''
+        systemctl status borgbackup-job-backupToArchimedes.service
+      '')
+    ]
+    ++ (let
+      borg = ''BORG_PASSCOMMAND="${BORG_PASSCOMMAND}" BORG_RSH="${BORG_RSH}" doas borg'';
+    in [
+      (pkgs.writeShellScriptBin "backup-mount" ''
+        ${borg} mount ${repo} /mnt/backup
+      '')
+      (pkgs.writeShellScriptBin "backup-umount" ''
+        doas borg umount /mnt/backup
+        doas rmdir /mnt/backup
+      '')
+      (pkgs.writeShellScriptBin "backup-list" ''
+        ${borg} list ${repo}
+      '')
+    ]);
 }
