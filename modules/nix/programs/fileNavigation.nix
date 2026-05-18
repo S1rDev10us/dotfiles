8a71b83442b79efbd50bb0f6ca63005fffb14928
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2026-05-18 13:05:26 +0100

    fix: add dolphin when not using kde and fix dolphin file associations

--- modules/nix/programs/fileNavigation.nix
+++ modules/nix/programs/fileNavigation.nix
@@ -7,4 +7,13 @@
   # Automount
   services.udisks2.enable = true;
   # Trash, ... support in thunar
+
+  environment.systemPackages = with pkgs; [
+    dolphin
+    baloo-widgets # baloo information in Dolphin
+    dolphin-plugins
+  ];
+  # Fix Dolphin file associations on non-Plasma desktop environments
+  # https://github.com/NixOS/nixpkgs/issues/409986
+  environment.etc."xdg/menus/applications.menu".source = "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
 }
