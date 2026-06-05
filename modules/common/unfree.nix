d5c0d8c12720a33b22cc4b05255cf786fe7d8286
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2026-06-05 01:09:54 +0100

    fix: allow functions in unfreePackages

--- modules/common/unfree.nix
+++ modules/common/unfree.nix
@@ -2,22 +2,20 @@
   config,
   lib,
   ...
-}: let
-  processedPackages =
-    builtins.map
-    (pkg:
-      if builtins.isString pkg
-      then pkg
-      else lib.getName pkg)
-    config.unfreePackages;
-in {
+}: {
   options.unfreePackages = lib.mkOption {
-    type = with lib.types; listOf (oneOf [package str]);
+    type = with lib.types; listOf (oneOf [package str (functionTo bool)]);
     default = [];
   };
   config = {
     nixpkgs.config.allowUnfree = lib.mkForce false;
     nixpkgs.config.allowUnfreePredicate = pkg:
-      builtins.elem (lib.getName pkg) processedPackages;
+      builtins.any (allowedPkg:
+        if builtins.isString allowedPkg
+        then (lib.getName pkg) == allowedPkg
+        else if lib.isFunction allowedPkg
+        then allowedPkg pkg
+        else (lib.getName pkg) == (lib.getName allowedPkg))
+      config.unfreePackages;
   };
 }
