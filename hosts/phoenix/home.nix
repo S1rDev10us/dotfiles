e01fddfba7e21aa373c2eed76eee14f31f93b084
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2026-07-09 20:05:45 +0100

    fix(host): reduce niri column widths for DP-2 on phoenix

--- hosts/phoenix/home.nix
+++ hosts/phoenix/home.nix
@@ -15,6 +15,18 @@
     ''
       spawn-at-startup "streamcontroller" "-b"
     ''
+    ''
+      output "DP-2" {
+        layout {
+          default-column-width { proportion 0.33333; }
+          preset-column-widths {
+            proportion 0.25
+            proportion 0.33333
+            proportion 0.5
+          }
+        }
+      }
+    ''
   ];
   home.packages = [
     inputs.tagstudio.packages.${pkgs.stdenv.hostPlatform.system}.tagstudio-jxl
